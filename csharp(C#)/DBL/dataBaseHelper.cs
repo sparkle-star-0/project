fd8b4768db2b560bdef19227f9bce83e21ece791
Amirhossein Sattari <amsattari1384@gmail.com>
2026-08-26 19:56:09 +0330

    final

---
+++ csharp(C#)/DBL/dataBaseHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+using System.Data;
+using System.Security.Cryptography;
+
+
+
+namespace shop.Models;
+
+public class dataBaseHelper
+{
+   private string connectionstring = "server=localhost;database=shop_db;uid=root;pwd=;charset=utf8mb4;";
+    public bool testconnect()
+    {
+        try
+        {
+            using (var connection = new MySqlConnection(connectionstring))
+            {
+                
+                connection.Open();
+                return true;
+               
+            }
+        }
+        catch (Exception ex)
+        {
+            //MessageBox.Show("connetionErorr" + ex.Message);
+            return false;
+        }
+    }
+    
+    
+   
+}
